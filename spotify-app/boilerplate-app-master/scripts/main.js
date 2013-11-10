22940dc7878f3fdaf28970487ae0c449b328542d
pagrawl3 <mail@prath.am>
2013-11-09 20:11:58 -0600

    some basic spotify integration

---
+++ spotify-app/boilerplate-app-master/scripts/main.js
@@ -0,0 +1,12 @@
+require([
+  '$api/models',
+  'scripts/language-example',
+  'scripts/cover-example',
+  'scripts/button-example',
+  'scripts/playlist-example'
+], function(models, languageExample, coverExample, buttonExample, playlistExample) {
+  'use strict';
+
+  
+
+});
